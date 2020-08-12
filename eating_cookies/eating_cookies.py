b22a2beba981ed44ec81277223ecaad6672a7b59
IsaiahFlax <flax.isaiah@gmail.com>
2020-08-12 02:56:37 -0600

    eating_cookies done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,20 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
-
+    # if n == 2:
+    #     return 2
+    arr = [0, 1, 1,]
+    while len(arr) < n +2:
+        arr2 = arr[-3:]
+        #print(arr2)
+        arr.append(sum(arr2))
+        #print(arr)
+    return arr[-1:].pop()
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(eating_cookies(50))
+    print(eating_cookies(100))
+    print(eating_cookies(500))
